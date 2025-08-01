b957c3aed87c39f60d582f40a2cd2d2bac3f6dc6
atharvapatle <atharvapatle001@gmail.com>
2025-08-01 16:30:30 +0530

    app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('templates\chat.html')
+    return render_template('chat.html')
 
-if __name__=='main':
-    app.run(host="0.0.0.0",port=8080,debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug= True)
