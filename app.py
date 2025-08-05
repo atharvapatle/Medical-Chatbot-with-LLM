38d162401314b07e586a1955662e709e1a871d1f
atharvapatle <atharvapatle001@gmail.com>
2025-08-05 11:36:41 +0530

    Deploy medical chatbot with all fixes

--- app.py
+++ app.py
@@ -6,52 +6,44 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-from src.prompt import *
+from src.prompt import system_prompt
 import os
-
 
 app = Flask(__name__)
 
-
 load_dotenv()
 
-PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
-OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 
-
 embeddings = download_hugging_face_embeddings()
 
-index_name = "medical-chatbot" 
+index_name = "medical-chatbot"
 
 docsearch = PineconeVectorStore.from_existing_index(
     index_name=index_name,
     embedding=embeddings
 )
 
-#retriver
-retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+# retriever
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
 
 chatModel = ChatOpenAI(model="moonshotai/kimi-k2:free")
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", system_prompt),
+    ("human", "{input}"),
+])
 
 question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
 rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-
-
 @app.route("/")
 def index():
     return render_template('chat.html')
-
-
 
 @app.route("/get", methods=["GET", "POST"])
 def chat():
@@ -62,7 +54,5 @@ def chat():
     print("Response : ", response["answer"])
     return str(response["answer"])
 
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
